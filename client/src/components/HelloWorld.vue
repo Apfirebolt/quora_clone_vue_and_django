8036efe424978f9d071d113e6b46d03b538f6266
Apfirebolt <aspper20@gmail.com>
2023-12-27 01:51:25 +0530

    minor fixes

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -9,12 +9,12 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ msg }} Hi Message King</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
+      Edit This Man
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
