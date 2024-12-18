07dc62df4908db14d7d9c00aa27b436cea1703e7
Apfirebolt <aspper20@gmail.com>
2024-12-18 11:18:34 +0530

    added form for question

---
+++ client/src/components/QuestionForm.vue
@@ -0,0 +1,59 @@
+<template>
+    <form class="space-y-6" @submit="handleSubmit">
+        <h2 class="text-lg text-center font-medium leading-6 text-white bg-primary p-4">
+            Question Form
+        </h2>
+        <div>
+            <label for="title" class="block text-sm font-medium text-gray-700">
+                Question Title
+            </label>
+            <div class="mt-1">
+                <input id="content" name="content" v-model="content" type="text" required="" placeholder="Enter Question" autocomplete="off"
+                    class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+            </div>
+        </div>
+
+        <div>
+            <label for="description" class="block text-sm font-medium text-gray-700">
+                Question Description
+            </label>
+            <div class="mt-1">
+                <textarea id="description" name="description" v-model="description" rows="4"
+                    placeholder="Enter Question Description"
+                    class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                </textarea>
+            </div>
+        </div>
+
+        <div class="text-center">
+            <button type="submit"
+                class="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-secondary hover:bg-primary focus:outline-none focus:ring-2 focus:ring-offset-2">
+                Add Question
+            </button>
+            <button @click="closeModal"
+                class="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white mx-2 bg-accent hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2">
+                Cancel
+            </button>
+        </div>
+    </form>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const props = defineProps({
+    closeModal: {
+        type: Function,
+        required: true
+    }
+});
+
+const content = ref('');
+const description = ref('');
+const errors = ref([]);
+
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log(content.value, description.value);
+    closeModal();
+}
+</script>
--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -8,6 +8,9 @@
             </h3>
             <div class="mt-2 max-w-xl text-sm text-gray-500">
                 <p>Change the email address you want associated with your account.</p>
+                <button @click="openModal" class="mt-3 inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                    Add Question
+                </button>
             </div>
             <p class="text-gray-700 my-3">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem minima et
@@ -16,10 +19,54 @@
                 commodi?
             </p>
         </div>
+        <TransitionRoot appear :show="isOpen" as="template">
+            <Dialog as="div" @close="closeModal" class="relative z-10">
+                <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0"
+                    enter-to="opacity-100" leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
+                    <div class="fixed inset-0 bg-black/25" />
+                </TransitionChild>
+
+                <div class="fixed inset-0 overflow-y-auto">
+                    <div class="flex min-h-full items-center justify-center p-4 text-center">
+                        <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
+                            enter-to="opacity-100 scale-100" leave="duration-200 ease-in"
+                            leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-95">
+                            <DialogPanel
+                                class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                <question-form :closeModal="closeModal" />
+                            </DialogPanel>
+                        </TransitionChild>
+                    </div>
+                </div>
+            </Dialog>
+        </TransitionRoot>
     </section>
     <footer-component />
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import QuestionForm from '../components/QuestionForm.vue';
+import {
+    TransitionRoot,
+    TransitionChild,
+    Dialog,
+    DialogPanel,
+    DialogTitle,
+} from '@headlessui/vue'
 
+const isOpen = ref(true);
+
+function closeModal() {
+    isOpen.value = false;
+    console.log('Modal opened')
+}
+
+function openModal() {
+    isOpen.value = true;
+}
+
+onMounted(() => {
+    console.log('Dashboard mounted');
+});
 </script>
